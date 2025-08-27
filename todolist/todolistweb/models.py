796adef6953fe9acc79212a3b8d7c59ce12357e4
nanini0 <alvaro.robles15081999@gmail.com>
2025-08-27 17:01:09 -0400

    notas, todolist y contact

---
+++ todolist/todolistweb/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+    
+
+class Notas(models.Model):
+    title= models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(blank=True)
+    phone = models.CharField(max_length=20, blank=True)
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
---
+++ todolist/todolistweb/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import Task,Notas # importamos models para ocupar la base de datos como objeto
+# Create your views here.
+
+def home(request):
+    return render(request,'home.html')
+
+def task_list(request):
+    tasks = Task.objects.all() #consulta todos los objetos de la base de datos Task creada en models
+    return render(request,"tasks/task_list.html",{"tasks":tasks}) 
+
+def task_add(request):
+    if request.method =="POST": # 1. Si el usuario envía el formulario
+        title = request.POST.get("title") # 2. Obtenemos el valor del input
+        if title:
+            Task.objects.create(title=title) # 3. Creamos tarea en BD
+        
+        return redirect("task_list") # nos devuelve a la pagina de lista de tareas
+    return render(request,"tasks/task_add.html")
+
+def task_complete(request, task_id):
+    task = get_object_or_404(Task, id=task_id) # 1. Buscar la tarea o dar error 404
+    task.completed = True   # 2. Cambiar estado
+    task.save()             # 3. Guardar en BD
+    return redirect("task_list")  # 4. Redirigir
+
+
+def task_delete(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect("task_list")
+
+
+##notas
+
+def nota_list(request):
+    notas = Notas.objects.all() #aqui le pasamos el objeto de notas
+    return render(request,"notas/notas_list.html", {"notas":notas})
+
+def notas_add(request):
+    if request.method=="POST":
+        title = request.POST.get("title")
+        if title:
+            Notas.objects.create(title=title)
+        
+        return redirect("notas_list")
+    return render(request,"notas/notas_add.html")
+
+def notas_delete(request, note_id):
+    notas= get_object_or_404(Notas, id=note_id)
+    notas.delete()
+    return redirect("notas_list")
+
+
+    
